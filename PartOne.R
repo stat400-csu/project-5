310a55270a4a75a93b470be34d911c5392a39ba7
Joseph Kroymann <jrkroymann10@gmail.com>
2020-12-02 22:04:49 -0600

    recreation of part one (pg. 395) work from paper

--- PartOne.R
+++ PartOne.R
@@ -1,3 +1,4 @@
+library(ggplot2)
 
 #Step One: Probabilities of Match Results
 
@@ -11,6 +12,60 @@ avg_home_concede <- 0
 
 avg_away_goal <- 0
 avg_away_concede <- 0
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------
+# re-creating example on pg. 395 (man city vs. man united, round 36, 2011-2012)
+
+# expected average goals
+ev_mc <- 1.842
+ev_mu <- 1.237
+
+# poisson distributed variables (function that returns probability of scoring a certain amount of goals)
+p_mc = function(goals) {
+  dpois(goals, ev_mc)
+}
+
+p_mu = function(goals) {
+  dpois(goals, ev_mu)
+}
+
+p_mc(1) # .292 (matches result from paper)
+p_mu(0) # .290 (matches result from paper)
+
+# probability of man city winning 1-0 (the two variables are independent of each other)
+p_mc(1) * p_mu(0) # .085 (matches result from paper)
+
+# plot of the two distributions (blue = man city, red = man united)
+x_s <- seq(0, 10)
+ggplot() + geom_line(aes(x_s, p_mc(x_s)), colour = "blue") + geom_line(aes(x_s, p_mu(x_s)), colour = "red") + xlab("goals") + ylab("density")
+
+# so, most goals ever scored in a premier league game is 9, so we can check all end-game scenarios up to 9?                                                                                                                              
+
+mc_win_prob <- 0
+mu_win_prob <- 0
+tie_prob <- 0
+goals = seq(0, 9)
+
+# double for loop calculating probabilities for all scenarios up to 9 goals each (i = mc, j = mu)
+for (i in goals) {
+  for (j in goals) {
+    if (i == j) {
+      tie_prob <- tie_prob + (p_mc(i)*p_mu(j))
+    }
+    if (i > j) {
+      mc_win_prob <- mc_win_prob + (p_mc(i)*p_mu(j))
+    }
+    if (i < j) {
+      mu_win_prob <- mu_win_prob + (p_mc(i)*p_mu(j))
+    }
+  }
+}
+
+# numbers are all similar to the ones on pg. 395 of the paper
+data.frame("mc win" = round(mc_win_prob, 3), "draw" = round(tie_prob, 3), "mu win" = round(mu_win_prob, 3))
+                                                                                                                                   
+#-------------------------------------------------------------------------------------------------------------------------------------------------
+
 
 #Probability of Each team winning
 #Should this be a function that we call in Step 2?
